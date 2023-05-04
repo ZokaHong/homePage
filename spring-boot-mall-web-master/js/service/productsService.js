415c8420c9183ffd8344a17cfba2455a1b8c9405
ZokaHong <zoka105105@gmail.com>
2023-05-04 15:14:01 +0800

    homePage

--- spring-boot-mall-web-master/js/service/productsService.js
+++ spring-boot-mall-web-master/js/service/productsService.js
@@ -1,5 +1,5 @@
 const serverUrl = 'https://spring-boot-mall-api-production.up.railway.app'
-const size = 6
+const size = 4
 
 let products = []
 let cart = []
@@ -89,11 +89,11 @@ function setProduct(response) {
     </div>
     `)
   })
-  $('.card').mouseover(function () { 
-    $(this).css('backgroundColor','#ffd6ba');
+  $('.card').mouseover(function () {
+    $(this).css('backgroundColor', '#81c3d7');
   });
-  $('.card').mouseout(function () { 
-    $(this).css('backgroundColor','#fff');
+  $('.card').mouseout(function () {
+    $(this).css('backgroundColor', '#fff');
   });
 
 
@@ -114,8 +114,8 @@ function setPages(response) {
 
   pagesEl.append(`
     <li id="previous" class="page-item">
-      <a class="page-link page-previous" style="background-color: #fff; font-size:24px; color: #000; cursor: pointer;" onclick="queryPreviousOrNext(${parseInt(page) - 1
-    })">前一頁</a>
+      <a class="page-link page-previous" style="background-color: #fff; font-size:24px; color: #000; cursor: pointer; px-2" onclick="queryPreviousOrNext(${parseInt(page) - 1
+    })"><i class="fa-solid fa-backward-step"></i></i></a>
     </li>
   `)
 
@@ -123,31 +123,39 @@ function setPages(response) {
   for (let pageNum = 1; pageNum < totalPage + 1; pageNum++) {
     pagesEl.append(`
       <li class="page-item" data-bs-toggle="tooltip" data-bs-placement="top" title="${pageNum}">
-        <a id="page_${pageNum}" class="page-link" style="background-color: #fff; font-size:24px; color: #000; cursor: not-allowed;" onclick="queryPage(this)">${pageNum}</a>
+        <a id="page_${pageNum}" class="page-link page-num" style="background-color: #fff; font-size:24px; color: #000; cursor: pointer;" onclick="queryPage(this)">${pageNum}</a>
       </li>
     `)
   }
+  
 
   pagesEl.append(`
     <li id="next" class="page-item">
       <a class="page-link page-next" style="background-color: #fff; font-size:24px; color: #000; cursor: pointer;" onclick="queryPreviousOrNext(${parseInt(page) + 1
-    })">後一頁</a>
+    })"><i class="fa-sharp fa-solid fa-forward-step"></i></a>
     </li>
   `)
-  $('.page-next').mouseover(function () { 
-    $('.page-next').css('backgroundColor','#ffd6ba');
+
+  $('.page-previous').mouseenter(function () {
+    $(this).css('backgroundColor', '#1282a2');
   });
-  $('.page-next').mouseout(function () { 
-    $('.page-next').css('backgroundColor','#fff');
+  $('.page-previous').mouseleave(function () {
+    $(this).css('backgroundColor', '#fff');
   });
-  $('.page-previous').mouseover(function () { 
-    $('.page-previous').css('backgroundColor','#ffd6ba');
-  });
-  $('.page-previous').mouseout(function () { 
-    $('.page-previous').css('backgroundColor','#fff');
+  $('.page-num').mouseenter(function () {
+    $(this).css('backgroundColor', '#1282a2');
   });
 
-
+  $('.page-num').mouseleave(function () {
+    $(this).css('backgroundColor', '#fff');
+  });
+  $('.page-next').mouseenter(function () {
+    $(this).css('backgroundColor', '#1282a2');
+  });
+  $('.page-next').mouseleave(function () {
+    $(this).css('backgroundColor', '#fff');
+  });
+  
 }
 
 function queryPreviousOrNext(clickedPage) {
@@ -158,20 +166,24 @@ function queryPreviousOrNext(clickedPage) {
 function queryPage(button) {
   page = button.innerText
   getProducts(page)
+  $('.page-num').removeClass('active');
 }
 
-function stylePageButton(totalPage) {
-  $('#page_' + page).addClass('active')
+// 還沒加進去
+// function stylePageButton(totalPage) {
+//   $('#page_' + page).addClass('active');
 
-  if (page == 1 && page == totalPage) {
-    $('#previous').addClass('disabled')
-    $('#next').addClass('disabled')
-  } else if (page == 1) {
-    $('#previous').addClass('disabled')
-  } else if (page == totalPage) {
-    $('#next').addClass('disabled')
-  }
-}
+//   if (page == 1 && page == totalPage) {
+//     $('#previous').addClass('disabled')
+//     $('#next').addClass('disabled')
+//   } else if (page == 1) {
+//     $('#previous').addClass('disabled')
+//   } else if (page == totalPage) {
+//     $('#next').addClass('disabled')
+//   }
+// }
+
+
 
 // $('#searchBtn').click(function (event) {
 //   event.preventDefault()
